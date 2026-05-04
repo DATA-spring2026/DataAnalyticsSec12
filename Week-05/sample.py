6c559c8d2c4e627fad3b781680084f362cf00d89
elizabeas <bess@rcc.team>
2026-05-04 11:00:05 -0500

    "Created sample first script"

---
+++ Week-05/sample.py
@@ -0,0 +1,34 @@
+# First test program
+
+# print("Hello world!")
+
+
+# Creating some variables for a structured greeting
+
+greeting = "Good morning"
+whom = "Student"
+
+print(greeting + " " + whom)
+
+print(greeting, whom)
+
+# List of variables for customer information
+
+firstname = "Lorah"
+lastname = "Samiel"
+zip = 55555
+state = "WI"
+city = 'Milwaukee'
+streetaddr = "55 O'Brien St"
+street2 = '55 O\'Brien St'   #this string won't work unless we have an escape character
+country = "US"
+favorite_song = '"I Will Survive"'
+print(favorite_song)
+print(street2)
+
+# output cusomter address
+
+print("CUSTOMER ADDRESS")
+print(firstname, lastname)
+print(streetaddr)
+print(city, state, zip)
