dd4eeafeaa980a08f863ec32d17b82bb261477dc
elizabeas <bess@rcc.team>
2026-05-04 11:35:33 -0500

    Updated budget script

--- Week-05/budget_example.py
+++ Week-05/budget_example.py
@@ -15,3 +15,22 @@ recurring_exp = rent + utilities + internet + groceries + credit_card
 print("Monthly income: " + str(income))
 print("Recurring expenses: " + str(recurring_exp))
 print("Income after expenses: " + str(income - recurring_exp))
+
+print()
+
+print("Estimated annual income: " + str(income * 12))
+# print("Estimated annual income: " + str(income) * 12)  --> if we cast income first, then multiply, we see different results
+
+print("Estimated annual income: " + str(income * 12))
+
+
+# testing out some conditional logic:
+
+# print(income == 3800)
+
+# a = 'apple'
+# aa = 'Apple'
+
+# print(a == aa) # checking for case sensitivity
+
+# ctrl+c is the shortcut to break the running of a program
