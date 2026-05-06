64f3f422a9193a40fde24b3d170558dc704c75cd
elizabeas <bess@rcc.team>
2026-05-06 11:19:19 -0500

    Practiced using input function

---
+++ Week-05/input_examples.py
@@ -0,0 +1,11 @@
+# working with the input function
+
+birthdate = input("Enter your birthday: ")
+
+print(f'You entered {birthdate}')
+print(type(birthdate))
+
+age = int(input("What is your age? "))
+
+print(f"Age: {age}")
+print(type(age))
